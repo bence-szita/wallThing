7296ca32ced4f490c13939fee50ed303f228acb2
Ben-sz <sz.bence53@gmail.com>
2020-07-09 16:30:38 +0200

    adds image to canvas

--- src/app/app-core/app-core.component.ts
+++ src/app/app-core/app-core.component.ts
@@ -15,16 +15,32 @@ export class AppCoreComponent implements OnInit{
  /*  @ViewChild('canvas', { static: true }) canvas: ElementRef<HTMLCanvasElement>; */
   public ctx: CanvasRenderingContext2D;
 
-  /* ctx.fillRect(25, 25, 150, 150); */
+  public framed_img = new Image();
 
   ngOnInit() {
+    this.framed_img.src = "/assets/images/goldberg-small.jpg";
     this.ctx = this.canvas.nativeElement.getContext('2d');
+  
+
     this.ctx.fillStyle = "#D74022";
-    this.ctx.fillRect(25, 25, 150, 150);  
+    this.ctx.fillRect(0, 0, 50, 50);  
+
+
+    this.framed_img.onload = () => {
+      // context.drawImage(framed_img, 0, 0);// this is line 14
+      // context.drawImage(framed_img, currentX-(framed_img.width/2), currentY-(framed_img.height/2), framed_img.width*imgScale.value, framed_img.height*imgScale.value);
+      console.log(this.framed_img.src);
+      this.ctx.drawImage(this.framed_img, 0, 0,  100, 100);
+   };
+
   }
 
-/*   this.ctx.fillStyle = "#D74022";
-  this.ctx.fillRect(25, 25, 150, 150);  */
+  _Go(){
+    this.ctx.drawImage(this.framed_img, 0, 100,  100, 100);
+  }
+
+
+
 
 
   constructor() {
