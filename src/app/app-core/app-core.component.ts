9336411734f5e9dbf17550f02badc879f359b0c6
Ben-sz <sz.bence53@gmail.com>
2020-07-09 14:44:21 +0200

    adds canvas

--- src/app/app-core/app-core.component.ts
+++ src/app/app-core/app-core.component.ts
@@ -1,13 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, ViewEncapsulation  } from '@angular/core';
 import { NgModule } from '@angular/core';
+
 
 @Component({
   selector: 'app-core',
   templateUrl: './app-core.component.html',
-  styleUrls: ['./app-core.component.css']
+  styleUrls: ['./app-core.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
-export class AppCoreComponent{
+export class AppCoreComponent implements OnInit{
 
-  constructor() { }
+  @ViewChild('canvas', {static: true})
+  canvas: ElementRef;
+ /*  @ViewChild('canvas', { static: true }) canvas: ElementRef<HTMLCanvasElement>; */
+  public ctx: CanvasRenderingContext2D;
+
+  /* ctx.fillRect(25, 25, 150, 150); */
+
+  ngOnInit() {
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+    this.ctx.fillStyle = "#D74022";
+    this.ctx.fillRect(25, 25, 150, 150);  
+  }
+
+/*   this.ctx.fillStyle = "#D74022";
+  this.ctx.fillRect(25, 25, 150, 150);  */
+
+
+  constructor() {
+   
+   }
 
 }
