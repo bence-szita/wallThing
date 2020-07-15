cd458c9b3365d8c875c162fc1b3a8b9ec2fff71a
Ben-sz <sz.bence53@gmail.com>
2020-07-16 01:26:39 +0200

    refactors logic to calculate image position depending on middle of the image. mobile to do

--- src/app/app-core/app-core.component.ts
+++ src/app/app-core/app-core.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef, ViewEncapsulation, Directive, HostListener, AfterViewInit  } from '@angular/core';
 import { NgModule } from '@angular/core';
 import { THIS_EXPR, ThrowStmt } from '@angular/compiler/src/output/output_ast';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-core',
@@ -33,6 +34,9 @@ export class AppCoreComponent implements OnInit{
   mySize: number;
   backgroundWidth: number;
   backgroundHeight: number;
+  actualFramedImgSize: Array<number>;
+  imageCenterPosition: Array<number>;
+  isImageRendered: boolean = false;
 
 
   ngOnInit() {
@@ -40,68 +44,56 @@ export class AppCoreComponent implements OnInit{
     this.framed_img.src = "https://images.unsplash.com/photo-1557218825-334e575bcc38?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjEzOTE3OH0";
     this.ctx = (this.canvas.nativeElement as HTMLCanvasElement).getContext('2d');
     const canvasID = document.getElementById('stage');
-    console.log(this.background);/* var canvasRect = this.ctx.canvas.getBoundingClientRect() */
-    console.log(this.canvas);
-/*     this.ctx.canvas.width= this.background_img.width;
-    this.ctx.canvas.height= this.background_img.height; */
-
     this.imageZoom = 0.5;
 
-
     this.background_img.onload = () => {
-      console.log("ezt", this.background.nativeElement.clientWidth);
-      console.log("ezt", this.background.nativeElement.scrollWidth);
       this.backgroundWidth = this.background.nativeElement.width;
       this.backgroundHeight = this.background.nativeElement.height;
-      
-
     }
 
+  
 
     this.framed_img.onload = () => {
       // context.drawImage(framed_img, currentX-(framed_img.width/2), currentY-(framed_img.height/2), framed_img.width*imgScale.value, framed_img.height*imgScale.value);
-      this.imagePosX = this.canvasWidth/2 - this.framed_img.width/2;
-      this.imagePosY = this.canvasHeight/2 - this.framed_img.height/2;
 
-/*      this.ctx.canvas.width =  this.framed_img.width;
-      
-      this.ctx.canvas.height=  this.framed_img.height; */
-
-      this.canvasWidth = this.ctx.canvas.clientWidth;
-      this.canvasHeight = this.ctx.canvas.clientHeight;
-
-      this.currentX = this.imagePosX;
-      this.currentY = this.imagePosY;
-      this.imageWidth = this.framed_img.width;
-      this.imageHeight = this.framed_img.height;
-
-      this._imgRender();
+    this.canvasWidth = this.ctx.canvas.clientWidth;
+    this.canvasHeight = this.ctx.canvas.clientHeight;
+      this.getImageSize();
+     /*  this.getImagePosition(); */
+    if (!this.isImageRendered){
+      this.imageCenterPosition = [this.canvasWidth/2,this.canvasHeight/2];
+    console.log("img center",this.imageCenterPosition ,this.canvasWidth/2 ,this.canvasHeight/2);
+      this.isImageRendered = true;
     }
+    console.log("after init img center",this.imageCenterPosition ,this.canvasWidth/2 ,this.canvasHeight/2);
+    
+
+    
+     /*  this._imgRender(); */
+    }
+ 
     
     canvasID.addEventListener('mousedown', (e) => {
       var canvasRect = this.ctx.canvas.getBoundingClientRect()
+
       var mouseX = Math.floor(e.pageX - canvasRect.x);
       var mouseY = Math.floor(e.pageY - canvasRect.y);
 
       /*  if ( mousex > ) */
-     this.cursorPosOnImageX = mouseX-this.currentX;
-     this.cursorPosOnImageY = mouseY-this.currentY;
-    
-/*      console.log("mousex", mouseX)
-     console.log("currentx", this.currentX)
-     console.log("cursor on img", this.cursorPosOnImageX)
-     console.log("img width", this.imageWidth)
- */
-
+     this.cursorPosOnImageX = -1*(this.imageCenterPosition[0] - mouseX);
+     this.cursorPosOnImageY = this.imageCenterPosition[1] - mouseY;
+  
       console.log(this.currentX);
-     if ((this.cursorPosOnImageX <= this.imageWidth ) &&
-          (this.cursorPosOnImageX > 0) &&
-          (this.cursorPosOnImageY <= this.imageHeight) &&
-          (this.cursorPosOnImageY > 0) 
+     if ((this.cursorPosOnImageX <= this.actualFramedImgSize[0]*0.5) &&
+          (this.cursorPosOnImageX >= this.actualFramedImgSize[0]*-0.5) &&
+          (this.cursorPosOnImageY <= this.actualFramedImgSize[1]*0.5) &&
+          (this.cursorPosOnImageY >= this.actualFramedImgSize[1]*-0.5) 
          ) {
               this.isImageMovable = true;
               console.log('touch start')
      }
+     console.log("e pagex, cavasrect, cursorpos", e.pageX, canvasRect.x, this.cursorPosOnImageX);
+     this._drawImage(this.framed_img);
 
    }); 
 
@@ -109,10 +101,14 @@ export class AppCoreComponent implements OnInit{
    /* getting coordinates of canvas box and gathering mouse coordinate relative to the canvas */
    if (this.isImageMovable){
     var canvasRect = this.ctx.canvas.getBoundingClientRect() /* to refactor: add resize element observer*/
-    this.currentX = Math.ceil(e.pageX - canvasRect.x - this.cursorPosOnImageX);
-    this.currentY = Math.ceil(e.pageY - canvasRect.y - this.cursorPosOnImageY);
+    console.log("e pagex, cavasrect, cursorpos", e.pageX, canvasRect.x, this.cursorPosOnImageX);
 
-  
+    this.imageCenterPosition[0] = Math.ceil(e.pageX - canvasRect.x - this.cursorPosOnImageX);
+    this.imageCenterPosition[1] = Math.ceil(e.pageY - canvasRect.y + this.cursorPosOnImageY);
+
+
+    console.log(this.imageCenterPosition[0]);
+    console.log(this.imageCenterPosition[1]);
       this.ResetCanvas()
       this._drawImage(this.framed_img);
     }
@@ -128,12 +124,11 @@ export class AppCoreComponent implements OnInit{
      this.cursorPosOnImageY = mouseY-this.currentY;
 
 
-     if ((this.cursorPosOnImageX <= this.imageWidth ) &&
+     if ((this.cursorPosOnImageX <= this.framed_img.width ) &&
      (this.cursorPosOnImageX > 0) &&
-     (this.cursorPosOnImageY <= this.imageHeight) &&
+     (this.cursorPosOnImageY <= this.framed_img.height) &&
      (this.cursorPosOnImageY > 0) ) {
           this.isImageMovable = true;
-          console.log('touch start')
     }
    });
 
@@ -164,17 +159,33 @@ export class AppCoreComponent implements OnInit{
     });
    };
 
+   getImageSize(){
+    this.actualFramedImgSize = [this.framed_img.width, this.framed_img.height].map(item => item * this.imageZoom) ;
+
+
+  }
+
+  getImagePosition(){
+    this.imagePosX = this.canvasWidth/2 - this.actualFramedImgSize[0]/2;
+    this.imagePosY = this.canvasHeight/2 - this.actualFramedImgSize[1]/2;
+    console.log("img pos x y", this.imagePosX, this.imagePosX)
+    this.currentX = this.imagePosX;
+    this.currentY = this.imagePosY;
+  }
+
+
+
    modifyZoom(zoomChanger){
     this.imageZoom += zoomChanger;
     this.ResetCanvas();
     this._imgRender();
+    this.getImageSize();
    }
   _imgRender(){
   /*   this.mouseEvents();
  */
     this._drawImage(this.framed_img)
   }
-
 
   ResetCanvas() {
     this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
@@ -183,11 +194,20 @@ export class AppCoreComponent implements OnInit{
   modifyImage(newImage){
     this.ResetCanvas();
     this.framed_img.src = newImage;
-/*     this._drawImage(this.framed_img) */
+     this._drawImage(this.framed_img) 
 
   }
   _drawImage(imageToRender){
-    this.ctx.drawImage(imageToRender, this.currentX, this.currentY, this.framed_img.width*this.imageZoom, this.framed_img.height*this.imageZoom);
+/* 
+    this.currentX = this.imageCenterPosition[0]-(this.framed_img.width*this.imageZoom)/2;
+    this.currentY = this.imageCenterPosition[1]-(this.framed_img.height*this.imageZoom)/2; */
+    this.ctx.drawImage(imageToRender, this.imageCenterPosition[0]-(this.framed_img.width*this.imageZoom)/2,
+                                      this.imageCenterPosition[1]-(this.framed_img.height*this.imageZoom)/2,
+                                      this.framed_img.width*this.imageZoom, this.framed_img.height*this.imageZoom);
+
+    console.log("after init img center",this.imageCenterPosition ,this.canvasWidth/2 ,this.canvasHeight/2);
+    console.log("imd width zoom",this.framed_img.width ,this.imageZoom)
+    console.log('ide draw', this.imageCenterPosition[0]-(this.framed_img.width*this.imageZoom)/2, this.imageCenterPosition[1]-(this.framed_img.height*this.imageZoom)/2 );
   }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { AppCoreComponent } from './app-core/app-core.component';
 import { ImageBrowserComponent } from './image-browser/image-browser.component';
 import { HttpClientModule } from '@angular/common/http';
-import { DataService } from './services/data.service'
+import { DataService } from './services/data.service';
+import { ImageUploadComponent } from './image-upload/image-upload.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     AppCoreComponent,
     ImageBrowserComponent,
+    ImageUploadComponent,
   ],
   imports: [
     BrowserModule,
